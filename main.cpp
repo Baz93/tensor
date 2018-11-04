06ea948a92f74fa4d205644ceb56f3a78629fc2a
Vasiliy Mokin <mokin93@ya.ru>
2018-11-04 16:05:10 +0300

    added test4, test5

--- main.cpp
+++ main.cpp
@@ -43,9 +43,25 @@ void test3() {
     assert((to_str(a) == "{{{1, 2, 3}}, {{4, 5, 6}}}"));
 }
 
+void test4() {
+    tensor<int, 3> a({2, 1, 3}, {1, 2, 3, 4, 5, 6});
+    a += a;
+    assert((a.shape == std::array<size_t, 3>{2, 1, 3}));
+    assert((to_str(a) == "{{{2, 4, 6}}, {{8, 10, 12}}}"));
+}
+
+void test5() {
+    tensor<int, 3> a({2, 1, 3}, {1, 2, 3, 4, 5, 6});
+    a = a + a[0];
+    assert((a.shape == std::array<size_t, 3>{2, 1, 3}));
+    assert((to_str(a) == "{{{2, 4, 6}}, {{5, 7, 9}}}"));
+}
+
 int main () {
     test1();
     test2();
     test3();
+    test4();
+    test5();
     return 0;
 }

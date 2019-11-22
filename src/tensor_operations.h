870a7b802b8167768e5f15f63dfb0eae1db11e98
Vasiliy Mokin <vasiliy.mokin@aimtech.com>
2019-11-22 15:14:10 +0300

    add tensor operations

---
+++ src/tensor_operations.h
@@ -0,0 +1,330 @@
+#pragma once
+
+#include "element_wise_operations.h"
+
+
+namespace tensors {
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator+=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 += v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator-=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 -= v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator*=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 *= v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator/=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 /= v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator%=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 %= v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator&=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 &= v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator|=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 |= v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator^=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 ^= v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator<<=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 <<= v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> tensor_subslice<T1, D1>& operator>>=(
+    tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    element_wise_apply([](T1 &v1, const T2 &v2) {
+        v1 >>= v2;
+    }, lhs, rhs);
+    return lhs;
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator+(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 + v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator-(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 - v2;
+    }, lhs, rhs);
+}
+
+template<typename T, size_t D> auto operator+(
+    const tensor_subslice<T, D> &val
+) {
+    return element_wise_calc([](const T &v) {
+        return +v;
+    }, val);
+}
+
+template<typename T, size_t D> auto operator-(
+    const tensor_subslice<T, D> &val
+) {
+    return element_wise_calc([](const T &v) {
+        return -v;
+    }, val);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator*(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 * v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator/(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 / v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator%(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 % v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator==(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 == v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator!=(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 != v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator>(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 > v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator<(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 < v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator>=(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 >= v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator<=(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 <= v2;
+    }, lhs, rhs);
+}
+
+template<typename T, size_t D> auto operator!(
+    const tensor_subslice<T, D> &val
+) {
+    return element_wise_calc([](const T &v) {
+        return !v;
+    }, val);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator&&(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 && v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator||(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 || v2;
+    }, lhs, rhs);
+}
+
+template<typename T, size_t D> auto operator~(
+    const tensor_subslice<T, D> &val
+) {
+    return element_wise_calc([](const T &v) {
+        return ~v;
+    }, val);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator&(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 & v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator|(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 | v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator^(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 ^ v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator<<(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 << v2;
+    }, lhs, rhs);
+}
+
+template<
+    typename T1, size_t D1, typename T2, size_t D2
+> auto operator>>(
+    const tensor_subslice<T1, D1> &lhs, const tensor_subslice<T2, D2> &rhs
+) {
+    return element_wise_calc([](const T1 &v1, const T1 &v2) {
+        return v1 >> v2;
+    }, lhs, rhs);
+}
+
+}  // namespace tensors
--- tensor.h
+++ tensor.h
@@ -2,4 +2,5 @@
 
 #include "src/tensor.h"
 #include "src/tensor_iterator.h"
+#include "src/tensor_operations.h"
 #include "src/undefs.h"
